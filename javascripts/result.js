9ccf0361a49c433adf5f0228f47c57861d4984d1
QVidad <70931116+QVidad@users.noreply.github.com>
2020-12-26 18:50:16 +0800

    Update result.js

--- javascripts/result.js
+++ javascripts/result.js
@@ -1,5 +1,17 @@
 window.addEventListener('load', () => {
-    
-    const search = sessionStorage.getItem('SEARCH');
-    document.getElementById('result-search').innerHTML = search;
+	const search = sessionStorage.getItem('SEARCH');
+	document.getElementById('result-search').innerHTML = search;
+
+	if (search == 'Apayao'||search == 'apayao'||search == 'Road'||search == 'road'){
+		document.getElementById('Apayao').style.visibility='visible';
+    	}
+	if (search == 'Aw-asen'||search == 'aw-asen'||search == 'Falls'||search == 'falls'){
+		document.getElementById('Aw-asen').style.visibility='visible';
+    	}
+	if (search == 'Kaparkan'||search == 'kaparkan'||search == 'Falls'||search == 'falls'){
+		document.getElementById('Kaparkan').style.visibility='visible';
+    	}
+	if (search == 'Pasaleng'||search == 'pasaleng'||search == 'Bay'||search == 'bay'){
+		document.getElementById('Pasaleng').style.visibility='visible';
+    	}
 })
